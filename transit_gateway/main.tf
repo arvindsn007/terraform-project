7160dd0092331d77b6d794e416d43780f9074e67
arvind <arvindsn.upsc@gmail.com>
2022-03-09 18:42:18 +0530

    transit gateway updated

--- transit_gateway/main.tf
+++ transit_gateway/main.tf
@@ -21,10 +21,7 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "my-test-transit-gateway-attac
   vpc_id             = "${var.vpc_id}"
   dns_support        = "enable"
 
-  subnet_ids = [
-    "${var.public_subnet1}",
-    "${var.public_subnet2}",
-  ]
+  subnet_ids = ["${var.public_subnet1}","${var.public_subnet2}"]
 
   tags =  {
     Name = "my-test-tgw-vpc-attachment"
